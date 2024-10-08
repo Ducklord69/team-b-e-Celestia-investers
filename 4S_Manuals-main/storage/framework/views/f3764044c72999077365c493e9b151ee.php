e0b7e9ca24c859ad26bbea7b91948773d90a3d70
christiancoremans <christiancoremans1@hotmail.com>
2024-10-08 21:02:11 +0200

    Footer compleet
    Compleet afgemaakte footer.
    
    contact page moet nog van andere usercase eentje bij als die af is.

--- 4S_Manuals-main/storage/framework/views/f3764044c72999077365c493e9b151ee.php
+++ 4S_Manuals-main/storage/framework/views/f3764044c72999077365c493e9b151ee.php
@@ -96,7 +96,8 @@
 
     </div>
 
-    <?php if (isset($component)) { $__componentOriginal71c6471fa76ce19017edc287b6f4508c = $component; } ?>
+</div>
+<?php if (isset($component)) { $__componentOriginal71c6471fa76ce19017edc287b6f4508c = $component; } ?>
 <?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.footer','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? (array) $attributes->getIterator() : [])); ?>
 <?php $component->withName('footer'); ?>
 <?php if ($component->shouldRender()): ?>
@@ -111,7 +112,6 @@
 <?php $component = $__componentOriginal71c6471fa76ce19017edc287b6f4508c; ?>
 <?php unset($__componentOriginal71c6471fa76ce19017edc287b6f4508c); ?>
 <?php endif; ?>
-</div>
 
 <!-- Bootstrap core JavaScript
 ================================================== -->
